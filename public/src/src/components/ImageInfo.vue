9167bb1cf65bb0fc0444e90bd0e7c4c00586e727
mpr0xy <mpr0xy@live.com>
2018-10-05 21:36:09 +0800

    src in

---
+++ public/src/src/components/ImageInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="ImageInfo card">
+    <img src="http://img2.ph.126.net/wdDNWHC3mOkMbyxoU_yQyA==/3312116051054550527.jpg" />
+    <img src="http://img2.ph.126.net/wdDNWHC3mOkMbyxoU_yQyA==/3312116051054550527.jpg" />
+    <p class="link">
+      <span>更多图文详情</span><img src="../assets/read-link.png" />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageInfo',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.ImageInfo {
+  position: relative;
+  font-size: 0px;
+  width: 750px;
+  img {
+    display: block;
+    width: 100%;
+    margin: 0 auto;
+  }
+  .link {
+    text-align: right;
+    font-size: 36px;
+    margin-top: 24px;
+    color: #f14a46;
+    img {
+      display: inline-block;
+      width: 16px;
+      margin-left: 10px;
+      vertical-align: middle;
+    }
+  }
+}
+</style>
---
+++ public/src/src/components/InviteTip.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="InviteTip" v-on:click="closeHandle">
+    <div class="mask"></div>
+    <img class="tip" src="../assets/share-tip.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JoinBar',
+  data () {
+    return {
+    }
+  },
+  props: [
+    'close'
+  ],
+  methods: {
+    closeHandle () {
+      this.close()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.InviteTip {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  .mask {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    background-color: rgba(0, 0, 0, 0.7);
+  }
+  .tip {
+    position: absolute;
+    top: 30px;
+    right: 80px;
+  }
+}
+</style>
